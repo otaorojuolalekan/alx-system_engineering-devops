a4e0d6beab67fd17bb424e4147705c78548c5920
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-26 15:02:48 +0100

    added scripts for puppet file creation

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet configuration - file creation
+
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
