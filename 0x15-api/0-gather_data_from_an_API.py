a1a2ed301050b6732e3d68989ee8e4e34c633f17
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-07-24 22:15:43 +0100

    fixed task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module contains solutions to API Project
+tasks.
+We are pulling and using APIs from
+"https://jsonplaceholder.typicode.com"
+"""
+import re
+import sys
+import requests
+
+# define the API url
+API = "https://jsonplaceholder.typicode.com"
+
+
+def main():
+    """show how many of how many tasks is done by user
+    User id is passed as an argument"""#
+    if len(sys.argv) > 1:
+        id = sys.argv[1]
+        if re.fullmatch(r"\d+", sys.argv[1]):
+            emp_res = requests.get('{}/users/{}'.format(API, id)).json()
+            emp_name = emp_res['name']
+            emp_todos = requests.get('{}/users/{}/todos'.format(API, id)).json()
+            completed_todos = requests.get('{}/users/{}/todos?completed=true'.format(API, id)).json()
+            no_todos = len(emp_todos)
+            no_completed = len(completed_todos)
+        print('Employee {} is done with tasks({}/{}):'.format(
+                emp_name,
+                no_completed,
+                no_todos
+                )
+            )
+        for todo in completed_todos:
+            print(f"\t {todo['title']}")
+
+if __name__ == '__main__':
+    main()
