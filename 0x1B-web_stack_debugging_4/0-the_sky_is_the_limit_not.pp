e03d57a8dcdc677bc906b32242f0950c05e1ced5
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-08-15 17:44:20 +0100

    readme and task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+$ulimit = 4096
+file { '/etc/default/nginx':
+  ensure  => present,
+  content => "ULIMIT=${ulimit}",
+  before => Exec['restart nginx'],
+}
+
+exec { 'restart nginx':
+  provider => shell,
+  command => 'sudo service nginx restart',
+}
