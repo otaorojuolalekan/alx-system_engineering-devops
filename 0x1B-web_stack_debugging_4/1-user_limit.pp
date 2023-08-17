337dc8e4cf251c12afed1de79dd4181d19ae5c54
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-08-17 15:02:12 +0100

    fixed puppet lint erros

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,12 @@
 # fix user ulimit
-
 exec{'replace5':
   provider => shell,
-  command => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/" /etc/security/limits.conf',
-
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace5'],
 }
 
 exec{'replace4':
   provider => shell,
-  command => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/" /etc/security/limits.conf',
-  after => Exec['replace5'],
+  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/" /etc/security/limits.conf',
 }
+
